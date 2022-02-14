1586551ecd8af88902797cb3478d2b2f833e59e8
Andr3Flor3s <andredemarquesflores@gmail.com>
2022-02-14 20:14:06 -0300

    aula 15

---
+++ aula15/vetornatela.js
@@ -0,0 +1,6 @@
+let vetor = [8,1,4,2,9]
+
+console.log(vetor)
+for(let pos = 0; pos < vetor.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${vetor[pos]}`)
+}
---
+++ aula15/vetornatela2.0.js
@@ -0,0 +1,6 @@
+let vetor = [8,1,4,2,9]
+
+console.log(vetor)
+for(let pos in vetor) {
+    console.log(`A posição ${pos} tem o valor ${vetor[pos]}`)
+}
